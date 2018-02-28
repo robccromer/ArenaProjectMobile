b41d91e8a7ea935ec99906d9fdcfa770462028af
robccromer <robcaryc@gmail.com>
2018-02-27 22:45:58 -0500

    Working with Fragments

--- app/src/main/java/com/example/robertcromerii/arenaproject/OperatorActivity.java
+++ app/src/main/java/com/example/robertcromerii/arenaproject/OperatorActivity.java
@@ -77,21 +77,21 @@ public class OperatorActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_operator_ManageUsers)
+        {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        }
+        else if (id == R.id.nav_operator_manageArenas)
+        {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        }
+        else if (id == R.id.nav_operator_manageGames)
+        {
 
         }
 
