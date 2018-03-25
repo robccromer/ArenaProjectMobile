bf838d1b04174f035326cf507f53f62353286a85
robccromer <robcaryc@gmail.com>
2018-03-25 18:38:26 -0400

    Adding League Owner

---
+++ app/src/main/java/com/example/robertcromerii/arenaproject/models/ArenaListData.java
@@ -0,0 +1,27 @@
+package com.example.robertcromerii.arenaproject.models;
+
+/**
+ * Created by Robert Cromer II on 3/6/2018.
+ */
+
+public class ArenaListData
+{
+    private String arenaName;
+    private int arenaID;
+    public ArenaListData(int arenaID, String arenaName) {
+        this.arenaName = arenaName;
+        this.arenaID = arenaID;
+    }
+    public String getArenaName() {
+        return arenaName;
+    }
+    public void setArenaName(String arenaName) {
+        this.arenaName = arenaName;
+    }
+    public int getArenaID() {
+        return arenaID;
+    }
+    public void setArenaID(int arenaID) {
+        this.arenaID = arenaID;
+    }
+}
